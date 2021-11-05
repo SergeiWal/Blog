990180b2e53883f2a9801b73a839144ddba31bd5
SergeiWal <korovaabc@gmail.com>
2021-11-05 16:45:34 +0300

    --commit--

--- blog/src/components/articleComments.tsx
+++ blog/src/components/articleComments.tsx
@@ -1,1 +1,24 @@
-export default function ArticleComments() {}
+import { FixedSizeList as List } from "react-window";
+import { cardHeight, cardMaxWidth } from "../constants/styles";
+import { ArticleComments, ListGeneratorParams } from "../types/articleTypes";
+
+export type ArticleCommentsProps = {
+  commets: ArticleComments;
+};
+
+const generateCommentRows = ({ data, index, style }: ListGeneratorParams) => {
+  return <div style={style}>{data[index]}</div>;
+};
+
+export default function ArticleComments() {
+  return (
+    <List
+      className="commentsList"
+      height={cardHeight * 2.5}
+      itemSize={cardHeight}
+      width={cardMaxWidth}
+    >
+      {generateCommentRows}
+    </List>
+  );
+}
